a123651156ca8a920dcff5b73044eb6e3b59d593
lehoanglong <lelong.ll32@gmail.com>
2024-05-25 17:19:36 +0700

    Create list products ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import "./css/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./page/Login";
 import LandingPage from "./page/LandingPage";
 import Dashboard from "./page/Dashboard";
 import Product from "./page/Product";
 import Layout from "./layouts/layout";
+import ScrollToTop from "./utils/ScrollToTop";
+import { ToastContainer } from "react-toastify";
 
 function App() {
+  const ROLE_SHOP = "ROLE_SHOP"
+  const ROLE_MEDICAL_CENTER = "ROLE_MEDICAL_CENTER"
+  const ROLE_AID_CENTER = "ROLE_AID_CENTER"
+  const ROLE_ADMIN = "ROLE_ADMIN"
+
+  const shopRoute = ({ element }) => {
+    const roleShop = sessionStorage.getItem("role").replace(/"/g, "")
+
+    if (roleShop === ROLE_SHOP) {
+      return element
+    } else {
+      return <Navigate to="/unauthorized" replace />
+    }
+  }
+
+
   return (
     <div className="App">
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
@@ -19,6 +38,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      <ToastContainer />
     </div>
   );
 }
